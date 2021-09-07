da3bdf8f20c7296d8a45501f427d85b52639e22c
Pedrolcb <87148319+Pedrolcb@users.noreply.github.com>
2021-09-07 16:22:16 -0300

    Vai dar certo

---
+++ src/pages/index.ts
@@ -0,0 +1,16 @@
+export {default as Cadastrar} from "./Cadastrar";
+export {default as CadastroFoto} from "./CadastroFoto";
+export {default as Config} from "./Config";
+export {default as Creditos} from "./Creditos"
+export {default as Detalhes} from "./Detalhes";
+export {default as Editar} from "./Editar";
+export {default as Escolha} from "./Escolha";
+export {default as Feedback} from "./Feedback";
+export {default as Galeria} from "./Galeria";
+export {default as MinhaGaleria} from "./MinhaGaleria";
+export {default as Login} from "./Login";
+export {default as Perfil} from "./Perfil";
+export {default as Camera} from "./Camera";
+
+
+
