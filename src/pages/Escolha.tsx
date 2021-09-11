b6c50ad2d98ab07300e13553cd0e623b18a151a7
Pedrolcb <87148319+Pedrolcb@users.noreply.github.com>
2021-09-10 22:39:48 -0300

    Pedro 10-09 noite

--- src/pages/Escolha.tsx
+++ src/pages/Escolha.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useNavigation } from "@react-navigation/core";
+//import { useNavigation } from "@react-navigation/core";
 import { Container } from "../styles/Escolha";
 import { ButtonEscolha, Header} from "../components";
+import { LoginTypes } from "../types/ScreenStack.types";
 
-export default function Escolha() {
-  const navigation = useNavigation();
+export default function Login({navigation}: LoginTypes) {
+
   function handleLogin() {
     navigation.navigate("Login");
   }
@@ -13,9 +14,9 @@ export default function Escolha() {
   }
   return (
     <Container>
-        <Header/> 
-        <ButtonEscolha title="LOG-IN" onPress={handleLogin} />
-        <ButtonEscolha title="CADASTRE-SE" onPress={handleCadastrar} />
+      <Header Image source={require("../../assets/logo.png")}/>
+      <ButtonEscolha title="LOG-IN" onPress={handleLogin} />
+      <ButtonEscolha title="CADASTRE-SE" onPress={handleCadastrar} />
     </Container>
   );
 }
