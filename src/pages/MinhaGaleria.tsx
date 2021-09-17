234bbc676be839b6b6a6e3a7276d134679af3a6d
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2021-09-17 10:24:10 -0300

    :)

--- src/pages/MinhaGaleria.tsx
+++ src/pages/MinhaGaleria.tsx
@@ -1,3 +1,42 @@
 import React from "react";
-import { useNavigation } from "@react-navigation/core";
-import { ButtonEscolha, Header} from "../components";
+import {
+    Container,
+    FormRow,
+    Label,
+    TextInput,
+    TitleFoto,
+} from "../styles/Editar";
+import { Image } from "react-native";
+import { Button , Header } from "../components";
+import { MinhaGaleriaTypes } from "../types/ScreenStack.types";
+
+export default function MinhaGaleria({navigation}: MinhaGaleriaTypes) {
+  function handleGaveta() {
+    navigation.navigate("Gaveta");
+  }
+  function handleEditar(){
+    navigation.navigate("Editar");
+  }
+  return (
+    
+    <Container>
+      <Header/>
+      <TitleFoto>Minha Galeria</TitleFoto>
+
+      <Image source={require("../../assets/imagemdeexemplo.png")}/>
+
+      <FormRow>
+        <Label>Título</Label>
+        <TextInput placeholder=""></TextInput>
+      </FormRow>
+
+      <Button onPress={handleEditar} />
+
+      <FormRow>
+       <Label>Descrição</Label>
+        <TextInput placeholder=""></TextInput>
+     </FormRow>
+    
+    </Container>
+  );
+}
