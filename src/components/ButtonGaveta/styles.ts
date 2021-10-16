6bcb1715af21bd5e0ea9887e8354ad074aec945a
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2021-10-16 18:46:31 -0300

    Update styles.ts

---
+++ src/components/ButtonGaveta/styles.ts
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components/native'
+import { ButtonStyleProps } from "../../interfaces/ButtonStyle.interface"
+
+const sizeVariations = {
+  default: css`
+  `,
+  define: css`
+    width: 120px;
+  `
+}
+
+export const ButtonStyle = styled.TouchableOpacity<ButtonStyleProps>`
+  background-color: #FEDF50;
+  margin-top: 50px;
+  font-size: 18px;
+  right: -235px;
+  color: black;
+  border-radius: 10px;
+  font-weight: 200;
+  margin-bottom: 80px;
+  top: -10%;
+  width: 90px;
+  height: 35px;
+  margin-right: 50px;
+  
+
+  ${(props) => sizeVariations[props.size || "default"]};
+`
+export const ButtonStyleText = styled.Text`
+  color: black;
+  text-align: center;
+  padding: 5px;
+  font-size: 18px;
+`
