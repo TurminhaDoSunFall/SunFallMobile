8760fd30ed82b6b08482407fe1bd4dccf10c8922
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2021-10-16 18:52:32 -0300

    Update Detalhes.tsx

--- src/pages/Detalhes.tsx
+++ src/pages/Detalhes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/core";
-import { ButtonEscolha, Header, Foto} from "../components";
+import { ButtonEscolha, Header, Foto , ButtonGaveta } from "../components";
 import { DetalhesTypes } from "../types/ScreenStack.types";
 import { Container, FormRow, Label } from "../styles/Detalhes";
 import { DetalhesProps } from "../interfaces/Detalhes.interface";
@@ -17,6 +17,7 @@ export default function Detalhes({navigation}: DetalhesTypes) {
     return (
       <Container>
         <Header/>
+        <ButtonGaveta title="Gaveta" onPress={handleGaveta} />
         <Foto image={require("../../assets/por_sol.jpg")}/>
         <FormRow>
           <Label>sLOREM IMPSUM</Label>
