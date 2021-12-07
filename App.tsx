d23ef1269ac43269b1112477e319e76f08b1d686
Pedrolcb <pedroluischagas134@gmail.com>
2021-12-07 15:10:04 -0300

    Confia

--- App.tsx
+++ App.tsx
@@ -2,19 +2,16 @@ import Routes from "./src/routes";
 import React from "react";
 import {useFonts, Nunito_400Regular} from "@expo-google-fonts/nunito";
 import {Chango_400Regular} from "@expo-google-fonts/chango";
-import {Text} from "react-native";
 import { AuthProvider } from "./src/hook/auth";
 
 export default function App() {
   let[ fontsLoaded ]=useFonts({Nunito_400Regular, Chango_400Regular});
   if (!fontsLoaded){
-    return <Text>Carregando~</Text>;
-  } else {
     return (
       <AuthProvider>
         <Routes />
       </AuthProvider>
     );
   }
-  
 }
+
--- src/services/data/index.ts
+++ src/services/data/index.ts
@@ -1,1 +1,2 @@
 export { default as apiUser } from './User';
+export { default as apiPerfil } from './Perfil';
